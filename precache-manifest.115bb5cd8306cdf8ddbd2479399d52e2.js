cd2df7727d9870dde747bb71ab423c4a78813143
Rishabh Singh <rishabh@hestabit.in>
2020-09-05 00:56:38 +0530

    Updates

---
+++ precache-manifest.115bb5cd8306cdf8ddbd2479399d52e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c8ef0928da25078d4c706c1a72d44e",
+    "url": "/react-redux-calculator/index.html"
+  },
+  {
+    "revision": "6f2d0ca3bb18b8d23857",
+    "url": "/react-redux-calculator/static/css/2.f2f6d01e.chunk.css"
+  },
+  {
+    "revision": "7b75624584ee9a4ed0ad",
+    "url": "/react-redux-calculator/static/css/main.2ff8e2f6.chunk.css"
+  },
+  {
+    "revision": "6f2d0ca3bb18b8d23857",
+    "url": "/react-redux-calculator/static/js/2.1b3315ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-calculator/static/js/2.1b3315ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b75624584ee9a4ed0ad",
+    "url": "/react-redux-calculator/static/js/main.add8f80f.chunk.js"
+  },
+  {
+    "revision": "f3cf5402a2fa728f616f",
+    "url": "/react-redux-calculator/static/js/runtime-main.477cf3a6.js"
+  },
+  {
+    "revision": "24fac62aed72b6200e3de04a9b17026a",
+    "url": "/react-redux-calculator/static/media/beyno.24fac62a.ttf"
+  }
+]);
